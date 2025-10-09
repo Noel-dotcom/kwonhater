6910fc5fbf574a8915aa237d94babf7528764660
Noel-dotcom <noelhannesdelun@gmail.com>
2025-10-09 12:29:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
+const message = `I miss you Lexi, I think about you and yearn for you everyday.\nI’m really sorry for everything and you probably hate me to the core, I don’t think you wanna talk to me either so I’m putting it here.\n\nI hope you’re doing well, I hope you’re not bullying Bri anymore. I pray for you everyday. I miss you and Bri.`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
