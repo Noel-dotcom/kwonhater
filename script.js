0b413b20b5166218594ba257ed1eda47060e7ec9
Noel-dotcom <noelhannesdelun@gmail.com>
2025-10-09 12:31:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `I miss you Lexi, I think about you and yearn for you everyday.\nI’m really sorry for everything and you probably hate me to the core, I don’t think you wanna talk to me either so I’m putting it here.\n\nI hope you’re doing well, I hope you’re not bullying Bri anymore. I pray for you everyday. I miss you and Bri.`;
+const message = `I miss you Lexi, I think about you and yearn for you everyday.\n\nI’m really sorry for everything and you probably hate me to the core, I don’t think you wanna talk to me either so I’m putting it here.\n\nI hope you’re doing well, I hope you’re not bullying Bri anymore. I pray for you everyday. I miss you and Bri.`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
